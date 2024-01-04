03fb12b2b8a4159697eac114bf1d66a4ab557e59
SantiGO055 <santigonzalez05@gmail.com>
2024-01-04 20:59:58 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Santiago Gonzalez on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
